38a5332717c04ed73a2fa371748e7e3b3b1a47f7
ElJefe111944 <jamesfarr1@sky.com>
2021-04-10 14:48:37 +0000

    Canonical url for post model created

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -26,6 +27,14 @@ class Post(models.Model):
         max_length=10, choices=STATUS_CHOICE, default='draft')
     objects = models.Manager()  # The default manager
     published = PublishedManager()  # The custom manager
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail', args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug])
 
 
 class Meta:
