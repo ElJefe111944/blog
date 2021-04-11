28f8fb7409583b20145c00e2cc243d4442f0708c
ElJefe111944 <jamesfarr1@sky.com>
2021-04-11 10:30:33 +0000

    post share view created

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
 
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)  # 3 post in each page
-#     page = request.GET.get('page')  # indicate current page number
-#     try:
-#         posts = paginator.page(page)  # obtain object from desired page
-#     except PageNotAnInteger:
-#         # if page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # if page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {
-#         'page': page,
-#         'posts': posts})
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {
+        'post': post, 'form': form})
 
 
 def post_detail(request, year, month, day, post):
