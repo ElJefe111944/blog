8e83df7f15b72f84f0746bdc63d42508849ecf26
ElJefe111944 <jamesfarr1@sky.com>
2021-04-11 11:35:55 +0000

    Post share url created

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,11 +3,13 @@ from django.views.generic import ListView
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_share(request, post_id):
     # retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         # form was submitted
         form = EmailPostForm(request.POST)
@@ -15,10 +17,16 @@ def post_share(request, post_id):
             # form fields passed validation
             cd = form.cleaned_data
             # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read" f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}'s \
+                 comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {
-        'post': post, 'form': form})
+        'post': post, 'form': form, 'sent': sent})
 
 
 def post_detail(request, year, month, day, post):
